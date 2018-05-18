de93295cc36afe4b1ce9b082faf4cfa0df81570a
junpeng.li <junpeng.li@CPX-M1VG23F1F87.dir.svc.accenture.com>
2018-05-18 14:12:16 +0900

    Log4j Completed

--- src/main/java/com/elearning/api/web/controller/PersonController.java
+++ src/main/java/com/elearning/api/web/controller/PersonController.java
@@ -1,5 +1,7 @@
 package com.elearning.api.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,13 @@ import com.elearning.api.service.PersonService;
 @RestController
 public class PersonController {
  
+	private Logger logger = LoggerFactory.getLogger(this.getClass()); 
+	
      @Autowired
      private PersonService personService ;
     
      public PersonController() {
+    	 logger.debug(" 我是 PersonController，我被实例化了 ");
        System. out .println(" 我是 PersonController，我被实例化了 " );
     }
  
